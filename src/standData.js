8146736e455fe85889c93549869d74d1f6808287
Hayley Robertson <hayleydawnrobertson@gmail.com>
2022-03-29 15:45:49 -0400

    add fake data

---
+++ src/standData.js
@@ -0,0 +1,40 @@
+const standData = [
+  {
+   id:1,
+   name: "Farm Stand 1",
+   address: {
+     street: "123 Main St",
+     town: "Anytown",
+     zip: "12345"
+   },
+   contact: {
+     tel: "555-555-5555",
+     url: "www.farmstand.com"
+
+   },
+   description: "This is a farm stand",
+   image: "https://farmstand.com/images/farmstand1.jpg",
+   produce: "this will be an array that is imported from elsewhere i think? larger dataframe, not in js"
+  },
+  
+ {
+    id:2,
+    name: "Farm Stand 2",
+    address: {
+      street: "123 Main St",
+      town: "Limington",
+      zip: "04049"
+    },
+    contact: { 
+      tel: "555-555-5555",
+      url: "www.farmstand.com"
+    },
+    description: "This is a farm stand",
+    image: "https://farmstand.com/images/farmstand2.jpg",
+    produce: "this will be an array that is imported from elsewhere i think? larger dataframe, not in js"
+
+ }, 
+    
+]
+
+export default standData;
---
+++ src/todo.md
@@ -0,0 +1,28 @@
+Near me distance filter
+
+connect to google analytics
+
+display farmers markets
+- contact page and bio?
+
+filter by open today/ weekends
+
+farm stands()
+  - <p>{name}
+    - <address props= street, town, zip  >
+    - <hours props= {isOpenToday}>
+    - <contact props= phone , url >
+    - <bio >*
+    - <product {product list}>*
+    - display <image> to the side- styled comp?
+    - props: address, hours, image, contact, bio, product, mapPin
+    - 
+  - </p>
+
+farmStandList()
+
+farmStand()
+  
+map of farms()
+  <interactiveMap> ??
+
